2e7dc02339d2227b5c6ce607f41bfc7c664f8c87
Liberty <49573531+lsLannan@users.noreply.github.com>
2024-02-09 11:16:59 +0000

    main created, draw grid function

---
+++ main.lua
@@ -0,0 +1,18 @@
+-- TETRIS
+
+-- draw game grid 
+function love.draw()
+    for y = 1, 18 do
+        for x = 1, 10 do
+            local blockSize = 20
+            local blockDrawSize = blockSize -1
+            love.graphics.rectangle (
+                'fill',
+                (x-1)*blockSize,
+                (y-1)*blockSize,
+                blockDrawSize,
+                blockDrawSize
+            )
+        end
+    end
+end
